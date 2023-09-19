6870ef31d6b04d3613d1375d3bc667294eb227ea
Mateus Camargo <mateuscamargo0@outlook.com.br>
2023-09-19 16:38:12 -0300

    fix(player): eventos não sendo adicionados no inicio

--- script.js
+++ script.js
@@ -8,24 +8,48 @@ class PlayerMusicComponent {
 
         this.currentTime = document.getElementById('current-time');
         this.duration = document.getElementById('duration');
+
+        this.nameMusic = document.getElementById('name-music');
+        this.artistNameValue = document.getElementById('artist-name-value');
+        this.albumNameValue = document.getElementById('album-name-value');
+        this.albumDateValue = document.getElementById('album-date-value');
+        this.albumGenreValue = document.getElementById('album-genre-value');
+        this.titleMusic = document.getElementById('title-music-value');   
     }
 
     play() {
         this.pausedStatus.innerHTML = ""
-        
-        this.eventListenerProgress()
-        this.eventListenerKeyPress()
-        
         audio.play();
     }
 
     pause() {
-        this.pausedStatus.innerHTML = "<b>(PAUSED)</b> "
+        this.pausedStatus.innerHTML = "<span>(PAUSED)</span> "
         audio.pause();
     }
 
     previous() {}
     next() {}
+
+    loadMusic(music) {
+        this.nameMusic.textContent = music.nameMusic;
+        this.artistNameValue.textContent = music.artistName;
+        this.albumNameValue.textContent = music.albumName;
+        this.albumDateValue.textContent = music.albumDate;
+        this.albumGenreValue.textContent = music.albumGenre;
+        this.titleMusic.textContent = music.titleMusic;
+
+        this.audio.src = music.src;
+    }
+
+    playerLoadQueue() {
+        const firstMusicmusics = musics[0];
+        this.loadMusic(firstMusicmusics);
+    }  
+
+    prepareComponent(){
+        this.eventListenerKeyPress()
+        this.eventListenerProgress()
+    }
     
     formatMediaTime(time) {
         const minutes = Math.floor(time / 60);
@@ -44,9 +68,9 @@ class PlayerMusicComponent {
     updateProgress(e) {
         const { duration, currentTime, params } = e.srcElement;
 
-        params.progressStatus.firstChild.textContent = `(${player.getProgress(currentTime, duration)}%)`;
-        params.currentTime.firstChild.textContent = player.formatMediaTime(currentTime);
-        params.duration.firstChild.textContent = player.formatMediaTime(duration);
+        params.progressStatus.firstChild.textContent = `(${params.getProgress(currentTime, duration)}%)`;
+        params.currentTime.firstChild.textContent = params.formatMediaTime(currentTime);
+        params.duration.firstChild.textContent = params.formatMediaTime(duration);
     }   
 
     switchCommands(e) {
@@ -84,4 +108,5 @@ class PlayerMusicComponent {
 }
 
 const player = new PlayerMusicComponent();
-player.play()
+player.playerLoadQueue();
+player.prepareComponent();
